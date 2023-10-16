d2c9b4e47b3fdc451431655932803ec75d78a5cc
Sohan Prabhath <soizsinghe@gmail.com>
2023-10-17 00:04:55 +0530

    signOut function initialization

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,10 +1,11 @@
+// ignore_for_file: avoid_print
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:smart_fit_on/models/UserModel.dart';
 
 class AuthServices {
   //register using mail and password
   //sign in using gmail
-  //sign out
 
   //firebase instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -17,5 +18,15 @@ class AuthServices {
   //create stream for cheking the auth changes in the user
   Stream<UserModel?> get user {
     return _auth.authStateChanges().map(_userWithFirebaseUserId);
+  }
+
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (err) {
+      print(err.toString());
+      return null;
+    }
   }
 }
